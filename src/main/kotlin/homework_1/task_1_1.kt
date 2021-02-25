e9d747609ab8cd62a744c8491c5bcc5da8fa2bc6
Maxim Esin <maksim.esin.2002@gmail.com>
2021-02-25 23:15:32 +0300

    initial_version

---
+++ src/main/kotlin/homework_1/task_1_1.kt
@@ -0,0 +1,32 @@
+package homework_1
+
+import java.util.Scanner;
+
+fun getIterativeFactorial(number: Int): Int {
+    var factorial = 1
+    for (i in 2 until number + 1)
+        factorial *= i
+    return factorial
+}
+
+fun getRecursiveFactorial(number: Int): Int {
+    return recursiveFactorial(1, number, 1)
+}
+
+fun recursiveFactorial(count: Int, factorialDegree: Int, result: Int): Int {
+    if (count <= factorialDegree)
+        return recursiveFactorial(count + 1, factorialDegree, result * count)
+    return result
+}
+
+fun enterUserNumber(): Int {
+    println("Enter number to process its factorial:")
+    val getNumber = Scanner(System.`in`)
+    return getNumber.nextInt()
+}
+
+fun main() {
+    val enteredNumber = enterUserNumber()
+    println("Iterative processing result: ${getIterativeFactorial(enteredNumber)}")
+    println("Recursive processing result: ${getRecursiveFactorial(enteredNumber)}")
+}
