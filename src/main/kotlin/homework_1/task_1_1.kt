fc2a0b92522ef61812ee3e1e7dddc5c197ff56f0
Maxim Esin <maksim.esin.2002@gmail.com>
2021-02-26 15:29:02 +0300

    class_update

--- src/main/kotlin/homework_1/task_1_1.kt
+++ src/main/kotlin/homework_1/task_1_1.kt
@@ -2,31 +2,28 @@ package homework_1
 
 import java.util.Scanner
 
-fun getIterativeFactorial(number: Int): Int {
-    var factorial = 1
-    for (i in 2 until number + 1)
-        factorial *= i
-    return factorial
-}
+class FactorialProcessor() {
+    fun getIterativeFactorial(number: Int): Int {
+        var factorial = 1
+        for (i in 2 until number + 1)
+            factorial *= i
+        return factorial
+    }
 
-fun getRecursiveFactorial(number: Int): Int {
-    return recursiveFactorial(1, number, 1)
-}
-
-fun recursiveFactorial(count: Int, factorialDegree: Int, result: Int): Int {
-    return if (count <= factorialDegree)
-        recursiveFactorial(count + 1, factorialDegree, result * count)
-    else result
-}
-
-fun enterUserNumber(): Int {
-    println("Enter number to process its factorial:")
-    val getNumber = Scanner(System.`in`)
-    return getNumber.nextInt()
+    fun getRecursiveFactorial(factorialDegree: Int, count: Int = 1, result: Int = 1): Int {
+        return if (count <= factorialDegree)
+            getRecursiveFactorial(factorialDegree, count + 1, result * count)
+        else result
+    }
 }
 
 fun main() {
-    val enteredNumber = enterUserNumber()
-    println("Iterative processing result: ${getIterativeFactorial(enteredNumber)}")
-    println("Recursive processing result: ${getRecursiveFactorial(enteredNumber)}")
+    println("Enter number to process its factorial:")
+
+    val scan = Scanner(System.`in`)
+    val enteredNumber = scan.nextInt()
+
+    val factorialProcessor = FactorialProcessor()
+    println("Iterative processing result: ${factorialProcessor.getIterativeFactorial(enteredNumber)}")
+    println("Recursive processing result: ${factorialProcessor.getRecursiveFactorial(enteredNumber)}")
 }
