7777a824a575b5993817e7711cd1fc5da8f4e14e
Esin Maksim <maksim.esin.2002@gmail.com>
2021-03-24 13:44:08 +0300

    update getter

--- src/main/kotlin/firsthomework/PerformedCommandStorage.kt
+++ src/main/kotlin/firsthomework/PerformedCommandStorage.kt
@@ -11,15 +11,12 @@ import kotlinx.serialization.json.Json
 /**
  * [PerformedCommandStorage] contains the list of whole numbers and the queue of actions on it
  * @property[actions] is an ordered private list of actions: [PushFront], [PushBack] and [MoveElement]
- * @property[localArrayDeque] is a private stack of whole numbers, modified by [actions]
+ * @property[arrayDeque] is a private stack of whole numbers, modified by [actions]
  */
 
 class PerformedCommandStorage {
     private val actions: Stack<Action> = Stack()
-    private val localArrayDeque: ArrayDeque<Int> = ArrayDeque()
-
-    val arrayDeque: ArrayDeque<Int>
-        get() = localArrayDeque
+    val arrayDeque: ArrayDeque<Int> = ArrayDeque()
 
     fun printCurrentPlacement() {
         /**
