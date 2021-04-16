791a3f9c2587ca60b46644d37f81bfa02036414a
Esin Maksim <maksim.esin.2002@gmail.com>
2021-04-16 13:32:07 +0300

    update syntax

--- src/main/kotlin/homework8/app.kt
+++ src/main/kotlin/homework8/app.kt
@@ -1,7 +1,17 @@
 package homework8
 
 import javafx.scene.control.Label
-import tornadofx.*
+import tornadofx.App
+import tornadofx.View
+import tornadofx.label
+import tornadofx.vbox
+import tornadofx.hbox
+import tornadofx.button
+import tornadofx.action
+import tornadofx.Controller
+import tornadofx.launch
+
+const val TABLE_SIZE = 3
 
 class MyApp : App(MyView::class)
 
@@ -9,17 +19,17 @@ class MyView : View("Hello") {
     private val controller: MyController by inject()
     private val field = mutableListOf<MutableList<String>>()
     private fun fillField() {
-        repeat(3) {
+        repeat(TABLE_SIZE) {
             field.add(mutableListOf("_", "_", "_"))
         }
     }
 
     override val root = vbox {
         fillField()
-        val gameConditionLabel = label ("game running")
-        for (i in 0..2) {
+        val gameConditionLabel = label("game running")
+        for (i in 0 until TABLE_SIZE) {
             hbox {
-                for (j in 0..2) {
+                for (j in 0 until TABLE_SIZE) {
                     button("_") {
                         action {
                             when (field[i][j]) {
@@ -40,18 +50,23 @@ class MyView : View("Hello") {
                 }
             }
         }
-
     }
 }
 
 class MyController : Controller() {
     fun isGameEnded(field: MutableList<MutableList<String>>): Boolean {
-        repeat(3) {
-            if (field[it][0] == field[it][1] && field[it][0] == field[it][2] && field[it][0] != "_") return true
-            if (field[0][it] == field[1][it] && field[0][it] == field[2][it] && field[0][it] != "_") return true
+        repeat(TABLE_SIZE) {
+            if ((field[it][0] == field[it][1] && field[it][0] == field[it][TABLE_SIZE - 1] && field[it][0] != "_")
+                || (field[0][it] == field[1][it] && field[0][it] == field[TABLE_SIZE - 1][it] && field[0][it] != "_"))  {
+                    return true
+            }
         }
-        if (field[0][0] == field[1][1] && field[0][0] == field[2][2] && field[0][0] != "_") return true
-        if (field[2][0] == field[1][1] && field[2][0] == field[0][2] && field[2][0] != "_") return true
+        if ((field[0][0] == field[1][1] && field[0][0] == field[TABLE_SIZE - 1][TABLE_SIZE - 1]
+                    && field[0][0] != "_")
+            || (field[TABLE_SIZE - 1][0] == field[1][1] && field[TABLE_SIZE - 1][0] == field[0][TABLE_SIZE - 1]
+                    && field[TABLE_SIZE - 1][0] != "_")) {
+            return true
+        }
         return false
     }
     fun endGame(gameConditionLabel: Label) {
