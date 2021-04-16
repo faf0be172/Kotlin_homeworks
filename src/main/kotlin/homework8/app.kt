ce3d543400ebaa2832bee88060507967b60f71ad
Esin Maksim <maksim.esin.2002@gmail.com>
2021-04-16 12:50:16 +0300

    first commit

---
+++ src/main/kotlin/homework8/app.kt
@@ -0,0 +1,36 @@
+package homework8
+
+import tornadofx.*
+
+class MyApp : App(MyView::class)
+
+class MyView : View("Hello") {
+    override val root = vbox {
+        val field = mutableListOf<MutableList<String>>()
+        repeat(3) {
+            field.add(mutableListOf("Empty", "Empty", "Empty"))
+        }
+        for (i in 0..2) {
+            for (j in 0..2) {
+                button("_") {
+                    action {
+                        when (field[i][j]) {
+                            "X" -> {
+                                this.text = "0"
+                                field[i][j] = "0"
+                            }
+                            else -> {
+                                this.text = "X"
+                                field[i][j] = "X"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    launch<MyApp>(args)
+}
