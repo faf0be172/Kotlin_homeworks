2675193f1a05599fd2f09b926cd45605fe479b9d
Esin Maksim <maksim.esin.2002@gmail.com>
2021-04-16 14:02:39 +0300

    update java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,8 @@ detekt {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.test {
