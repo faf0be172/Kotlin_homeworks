04c6cb4561d8142b2ace42f59df8158bafe6f382
Esin Maksim <maksim.esin.2002@gmail.com>
2021-04-16 13:18:33 +0300

    update game condition

--- src/main/kotlin/homework8/app.kt
+++ src/main/kotlin/homework8/app.kt
@@ -1,15 +1,22 @@
 package homework8
 
+import javafx.scene.control.Label
 import tornadofx.*
 
 class MyApp : App(MyView::class)
 
 class MyView : View("Hello") {
-    override val root = vbox {
-        val field = mutableListOf<MutableList<String>>()
+    private val controller: MyController by inject()
+    private val field = mutableListOf<MutableList<String>>()
+    private fun fillField() {
         repeat(3) {
-            field.add(mutableListOf("Empty", "Empty", "Empty"))
+            field.add(mutableListOf("_", "_", "_"))
         }
+    }
+
+    override val root = vbox {
+        fillField()
+        val gameConditionLabel = label ("game running")
         for (i in 0..2) {
             hbox {
                 for (j in 0..2) {
@@ -25,11 +32,30 @@ class MyView : View("Hello") {
                                     field[i][j] = "X"
                                 }
                             }
+                            if (controller.isGameEnded(field)) {
+                                controller.endGame(gameConditionLabel)
+                            }
                         }
                     }
                 }
             }
         }
+
+    }
+}
+
+class MyController : Controller() {
+    fun isGameEnded(field: MutableList<MutableList<String>>): Boolean {
+        repeat(3) {
+            if (field[it][0] == field[it][1] && field[it][0] == field[it][2] && field[it][0] != "_") return true
+            if (field[0][it] == field[1][it] && field[0][it] == field[2][it] && field[0][it] != "_") return true
+        }
+        if (field[0][0] == field[1][1] && field[0][0] == field[2][2] && field[0][0] != "_") return true
+        if (field[2][0] == field[1][1] && field[2][0] == field[0][2] && field[2][0] != "_") return true
+        return false
+    }
+    fun endGame(gameConditionLabel: Label) {
+        gameConditionLabel.text = "game ended"
     }
 }
 
