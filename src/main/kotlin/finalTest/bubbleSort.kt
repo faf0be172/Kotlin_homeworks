f337ed290d33489c31de3ef034fdbc571e9df01c
Esin Maksim <maksim.esin.2002@gmail.com>
2021-05-28 12:57:40 +0300

    update syntax

--- src/main/kotlin/finalTest/bubbleSort.kt
+++ src/main/kotlin/finalTest/bubbleSort.kt
@@ -7,13 +7,17 @@ import java.lang.IllegalArgumentException
  * @return [bubbleSorted] sorted iterable collection
  */
 
-private fun <T> checkComparator(sortingCollection: MutableList<T>, comparator: Comparator<T>) {
+private fun <T> checkNullsComparator(sortingCollection: MutableList<T>, comparator: Comparator<T>) {
     if (sortingCollection.size > 0) {
         val comparingResult = comparator.compare(sortingCollection[0], sortingCollection[0])
         if (comparingResult != 0) {
             throw IllegalArgumentException("Your comparator return non-null value for similar elements")
         }
     }
+}
+
+private fun <T> checkUnequalsElementsComparator(sortingCollection: MutableList<T>, comparator: Comparator<T>) {
+
     if (sortingCollection.size > 1) {
         val firstComparing = comparator.compare(sortingCollection[0], sortingCollection[1])
         val secondComparing = comparator.compare(sortingCollection[1], sortingCollection[0])
@@ -29,7 +33,8 @@ private fun <T> checkComparator(sortingCollection: MutableList<T>, comparator: C
 fun <T> bubbleSorted(collection: Iterable<T>, comparator: Comparator<T>): Iterable<T> {
     var isSwapped = true
     val sortingCollection = collection.toMutableList()
-    checkComparator(sortingCollection, comparator)
+    checkNullsComparator(sortingCollection, comparator)
+    checkUnequalsElementsComparator(sortingCollection, comparator)
 
     while (isSwapped) {
         isSwapped = false
